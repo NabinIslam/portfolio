c3d926adab412d3e9aeb240593ff35548dc43e6e
Nabin Islam <sinabin62246@gmail.com>
2024-04-06 03:34:28 +0600

    added 3rd project

--- src/components/MyPortfolio.tsx
+++ src/components/MyPortfolio.tsx
@@ -1,32 +1,7 @@
+import { projects } from '@/data/projects';
 import ProjectCard from './ProjectCard';
 
 const MyPortfolio = () => {
-  const projects: {
-    imageSrc: string;
-    projectTitle: string;
-    techUsed: string;
-    liveLink: string;
-    clientLink: string;
-    serverLink: string;
-  }[] = [
-    {
-      imageSrc: '/kinun_ss.png',
-      projectTitle: 'Kinun | Ecommerce Website',
-      techUsed: 'React, Tailwind, Node, Express, MongoDB',
-      liveLink: 'https://kinun-2563f.web.app/',
-      clientLink: 'https://github.com/NabinIslam/kinun-react-ecommerce-client',
-      serverLink: 'https://github.com/NabinIslam/kinun-react-ecommerce-server',
-    },
-    {
-      imageSrc: '/blogify_ss.png',
-      projectTitle: 'Blogify | Blog Website',
-      techUsed: 'Next.js, Tailwind, Node, Express, MongoDB',
-      liveLink: 'https://blogify-blog.vercel.app/',
-      clientLink: 'https://github.com/NabinIslam/Blogify-blog-app-Next.js',
-      serverLink: 'https://github.com/NabinIslam/blogify-blog-app-server',
-    },
-  ];
-
   return (
     <section id="aboutMe" className="bg-[#191C26]">
       <div className="container py-24">
---
+++ src/data/projects.ts
@@ -0,0 +1,35 @@
+export const projects: {
+  imageSrc: string;
+  projectTitle: string;
+  techUsed: string;
+  liveLink: string;
+  clientLink: string;
+  serverLink: string;
+}[] = [
+  {
+    imageSrc: '/kinun_ss.png',
+    projectTitle: 'Kinun | Ecommerce Website',
+    techUsed: 'React, Tailwind, Node, Express, MongoDB',
+    liveLink: 'https://kinun-2563f.web.app/',
+    clientLink: 'https://github.com/NabinIslam/kinun-react-ecommerce-client',
+    serverLink: 'https://github.com/NabinIslam/kinun-react-ecommerce-server',
+  },
+  {
+    imageSrc: '/blogify_ss.png',
+    projectTitle: 'Blogify | Blog Website',
+    techUsed: 'Next.js, Tailwind, Node, Express, MongoDB',
+    liveLink: 'https://blogify-blog.vercel.app/',
+    clientLink: 'https://github.com/NabinIslam/Blogify-blog-app-Next.js',
+    serverLink: 'https://github.com/NabinIslam/blogify-blog-app-server',
+  },
+  {
+    imageSrc: '/task-management-app.png',
+    projectTitle: 'Task Management App',
+    techUsed: 'React.js, Tailwind, Node, Express, MongoDB',
+    liveLink: 'https://cd-task-management.web.app/',
+    clientLink:
+      'https://github.com/NabinIslam/coder-squad-task-management-client',
+    serverLink:
+      'https://github.com/NabinIslam/coder-squad-task-management-server',
+  },
+];
